6ce4f1593310cd2ff7e3b47e05bd54d18cd0d007
FreakJoe <sandmensch933@web.de>
2016-08-15 19:53:52 +0200

    Added database functionality, changed some form styling

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class CreateArticleForm(Form):
 	title = StringField('title', validators=[DataRequired()])
-	content = StringField('content', validators=[DataRequired()])
+	content = TextAreaField('content', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,23 +1,20 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db, models
 from .forms import CreateArticleForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	entries = [
-		{
-			'title': 'Hello',
-			'content': 'Lorem ipsum..'
-		}
-	]
+	entries = models.Article.query.all()
 	return render_template('index.html', entries=entries)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
 	form = CreateArticleForm()
 	if form.validate_on_submit():
-		print('%s %s' % (form.title.data, form.content.data))
+		a = models.Article(title=form.title.data, content=form.content.data)
+		db.session.add(a)
+		db.session.commit()
 		return redirect(url_for('index'))
 		
 	return render_template('create.html', form=form)
